6f7f91fcda8fb67b231d2eb7c32b980155e1ee6e
Adeyemo Oluwatosin Maryam <omadeyemo@gmail.com>
2022-09-14 19:13:05 +0100

    initial commit

---
+++ 0x02-functions_nested_loops/alpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * printAlpha - prints alphabet except c and k
+ * 
+ * Return: void
+ */
+
+void printAlpha(void)
+{
+    int i;
+
+    for (i = 'a'; i <= 'z'; i++)
+    {
+        if (i == 'c' || i == 'k')
+                continue;
+            _putchar(i);
+            _putchar('\n');
+    }
+    _putchar('\n');
+}
+ 
+ 
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - calls other func
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+    printAlpha();
+    putchar();
+    int main(void);
+    
+    return (0);
+}
